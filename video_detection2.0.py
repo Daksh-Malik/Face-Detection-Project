a3d4ba966652a79e4649fe07f96742d1d1c7bbe2
DakshMalik02 <daksh0399.be21@chitkara.edu.in>
2024-03-29 00:42:24 +0530

    Video Detection 2.0 Added

--- video_detection2.0.py
+++ video_detection2.0.py
@@ -3,6 +3,7 @@ import cv2
 face_classifier = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
+smileCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_smile.xml')
 
 #accessing webcam (0 is webcam)
 video_capture = cv2.VideoCapture(0)
@@ -13,8 +14,23 @@ def detect_bounding_box(vid):
     faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
     for (x, y, w, h) in faces:
         cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
-        cv2.putText(vid, "I am a Human", (x, y + h + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    
+        roi_gray = gray_image[y:y+h, x:x+w]
+        roi_color = vid[y:y+h, x:x+w]
+        smile = smileCascade.detectMultiScale(
+        roi_gray,
+        scaleFactor= 1.5,
+        minNeighbors=15,
+        minSize=(25, 25),
+        )
+        
+        for (xx, yy, ww, hh) in smile:
+            cv2.rectangle(roi_color, (xx, yy), (xx + ww, yy + hh), (0, 255, 0), 2)
+        
+        if(len(smile)>0):
+            cv2.putText(vid, "I am a Happy Human", (x, y + h + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        else:
+            cv2.putText(vid, "I am a Human", (x, y + h + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        
     return faces
 
 #real time face detecting
